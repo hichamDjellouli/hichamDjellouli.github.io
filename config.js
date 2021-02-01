8874ccbeb8db9214ff571329fd1d19eb8545d09d
HICHAM DJELLOULI <h_djellouli@esi.dz>
2021-02-01 19:15:55 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
     IPConfig.$inject = ['$rootScope'];
     function IPConfig($rootScope) {
         var service = {};
-        $rootScope.main_url = "http://127.0.0.1:8080/";
-        $rootScope.api_url = "http://127.0.0.1:3000/api/";
+        $rootScope.main_url = "http://dzental.me/";
+        $rootScope.api_url = "http://dzental.me:3000/api/";
         return service;
     }
 })();
